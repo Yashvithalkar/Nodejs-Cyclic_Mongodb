07e890fd21a16367d373c5e66decca8201ae5ddb
yash vithalkar <vithalkaryash2002@gmail.com>
2023-08-11 14:55:35 +0530

    inity

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const Book = require("./models/books");
+const path = require("path");
+const hbs = require("hbs");
+
+
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+
+const static_path = path.join(__dirname,"./public");
+const template_path = path.join(__dirname,"./template/views");
+const partials_path = path.join(__dirname,"./template/partials");
+
+
+app.use(express.static(static_path));
+app.set("view engine","hbs"); // give permission to use hbs and views folder 
+app.set("views",template_path);// he not gate views folder so we get him path
+hbs.registerPartials(partials_path); //
+
+
 
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
@@ -19,39 +36,52 @@ const connectDB = async () => {
 }
 
 //Routes go here
-app.get('/', (req,res) => {
-    res.send({ title: 'Books' });
-})
+// app.get('/', (req,res) => {
+//     res.send({ title: 'Books' });
+// })
 
-app.get('/books', async (req,res)=> {
+// app.get('/books', async (req,res)=> {
 
-  const book = await Book.find();
+//   const book = await Book.find();
 
-  if (book) {
-    res.json(book)
-  } else {
-    res.send("Something went wrong.");
-  }
+//   if (book) {
+//     res.json(book)
+//   } else {
+//     res.send("Something went wrong.");
+//   }
   
-});
+// });
 
-app.get('/add-note', async (req,res) => {
-  try {
-    await Book.insertMany([
-      {
-        title: "bajoriya",
-        body: "Body text goes here...",
-      },
-      {
-        title: "Games of Thrones",
-        body: "Body text goes here...",
-      }
-    ]);
-    res.json({"Data":"Added"})
-  } catch (error) {
-    console.log("err", + error);
-  }
-})
+// app.get('/add-note', async (req,res) => {
+//   try {
+//     await Book.insertMany([
+//       {
+//         title: "bajoriya",
+//         body: "Body text goes here...",
+//       },
+//       {
+//         title: "Games of Thrones",
+//         body: "Body text goes here...",
+//       }
+//     ]);
+//     res.json({"Data":"Added"})
+//   } catch (error) {
+//     console.log("err", + error);
+//   }
+// })
+
+app.get("/", (req,res) => {
+ 
+  res.render("index")
+  
+  });
+
+
+  app.get("/register", (req,res) => {
+ 
+    res.render("register")
+    
+    });
 
 //Connect to the database before listening
 connectDB().then(() => {
